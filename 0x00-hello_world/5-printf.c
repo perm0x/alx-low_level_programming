6aca44e8933c46bc00551bc65424d21ac7a2f868
perm0x <perm3loo@gmail.com>
2022-10-12 23:08:45 -0700

    done 1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void){
-	printf("with proper grammar, but the outcome is a piece");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return(0);
-}
+}(
