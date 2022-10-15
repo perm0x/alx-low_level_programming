a8a984289a1ca4d9298e703fa12597a7badfffc8
perm0x <perm3loo@gmail.com>
2022-10-15 05:19:29 -0700

    done

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,16 @@
-#include <stdio.h> 
- /** 
-  * main - entry point 
-  * 
-  * Return: alway returns 0 
-  * 
-  */ 
- int main(void) 
- { 
-         char a; 
-  
-         for (a = 'z'; a >= 'a' ; a--) 
-                 putchar(a); 
-         putchar('\n'); 
-         return (0); 
- }
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a' ; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
